8db26b5a26bdfa2d689ab9547bf35d083506bebf
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-06-14 13:01:14 +0900

    feat: 회원 등급을 저장하는 변수 선언 및 출력 (09.js)

--- week01/01_variables/09.js
+++ week01/01_variables/09.js
@@ -1,1 +1,11 @@
 // 9. "회원 등급('VIP', '골드', '실버' 중 하나)"을 담는 변수를 작성해보세요.
+let membershipTier = '';
+
+membershipTier = 'VIP';
+console.log('회원님의 등급은 ' + membershipTier + '입니다! 🎉');
+
+membershipTier = '골드';
+console.log('회원님의 등급은 ' + membershipTier + '입니다! 🌟');
+
+membershipTier = '실버';
+console.log('회원님의 등급은 ' + membershipTier + '입니다! 🪙');
