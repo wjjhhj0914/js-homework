2fa4edc3d0db453456940497962aeec8c8b5aa1d
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-06-14 12:52:45 +0900

    feat: 댓글 수 저장하는 변수 선언 및 출력 (07.js)

--- week01/01_variables/07.js
+++ week01/01_variables/07.js
@@ -1,1 +1,7 @@
 // 7. 사용자가 작성한 "댓글 수(예: 12)"를 담는 변수를 선언하고 값을 할당하세요.
+let commentCount;
+commentCount = 12;
+console.log('내가 작성한 댓글 수 : ' + commentCount);
+
+commentCount = 22;
+console.log('내가 작성한 댓글 수 : ' + commentCount);
